496334903b8c71d6792204e136f7354b6dea38a3
mr-andersen523 <chasea523@gmail.com>
2020-04-09 23:47:49 -0700

    added logo to nav, button, made comments 4 review

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,25 +1,28 @@
 import React from "react";
 import {Menu, Button} from "semantic-ui-react";
 
-import StaffLogo from "../images/Service Staff/servicestaff1.png";
+import StaffLogo from "../images/Service Staff/servicestaff2.png";
+// import StaffLogo from "../images/Service Staff/SSSBplain.png";
 
 function TopNav() {
   return(
-    <div>
     <Menu borderless inverted fluid fixed="top">
-          <Menu.Item>
-
-            {/* TODO: add image logo */}
-          {/* <img src={StaffLogo} style={{width:'100px', height:'40px'}}/> */}
-          
-          </Menu.Item>
+          <Menu.Item header as="a">
+          <img src={StaffLogo} style={{width:'100px', height:'35px'}}/></Menu.Item>
+          {/*Below is style for servicestaff2.png --- you would just need to change the Staff logo import above  */}
+          {/* <img src={StaffLogo} style={{width:'50px', height:'20px'}}/></Menu.Item> */}
+          {/* <Menu.Item>Service Staff Strike Back</Menu.Item> */}
           <Menu.Menu position="right">
             <Menu.Item href = "/" as="a">Home</Menu.Item>
-            <Menu.Item href="/login" as="a"><Button>Login</Button></Menu.Item>
-            <Menu.Item href="/signup" as="a"><Button primary>Sign up</Button></Menu.Item>
+            <Menu.Item>   
+              <Button.Group>
+                  <Button href="/login" as="a">Login</Button>
+                    <Button.Or />
+                  <Button primary href="/signup" as="a">Signup</Button>
+              </Button.Group>
+            </Menu.Item>
           </Menu.Menu>
         </Menu>
-    </div>
   )
   }
 
